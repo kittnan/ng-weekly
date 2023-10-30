6fdc8a93530f32e89315bc062aff26d04f737b63
kittinan-k <kittinan-k@kyocera.co.th>
2023-10-30 16:21:08 +0700

    k

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-import { Location } from '@angular/common';
+import { Location, DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +10,11 @@ import { Location } from '@angular/common';
 export class AppComponent {
   title = 'ng-weekly';
 
-  constructor(private router: Router, private _location: Location) {}
+  constructor(
+    private router: Router,
+    private _location: Location,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -36,9 +40,27 @@ export class AppComponent {
         showConfirmButton: false,
         timer: 1500,
       }).then(() => {
-        this._location.back();
-        // this.router.navigate(['chart/calculate']);
+        // this._location.back();
+        this.router.navigate(['chart/calculate']);
       });
+    }
+  }
+
+  scrollToTop() {
+    // Scroll to the top of the page
+    this.document.body.scrollTop = 0; // For Safari
+    this.document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Here you can define when to show or hide the button to scroll to the top
+    // Example: Show button after scrolling 100px down
+    const yOffset =
+      window.pageYOffset || this.document.documentElement.scrollTop;
+    if (yOffset > 100) {
+      // Display your button or implement logic to show the scroll to top option
+    } else {
+      // Hide your button or implement logic to hide the scroll to top option
     }
   }
 }
