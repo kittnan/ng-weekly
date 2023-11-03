7c55a6b1d85c274d79ee7def09024407bbeebda4
kittinan-k <kittinan-k@kyocera.co.th>
2023-11-03 11:08:43 +0700

    า

--- src/app/pages/chart/view/view.component.ts
+++ src/app/pages/chart/view/view.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CalculateService } from 'src/app/https/calculate.service';
 import { ChartHttpService } from 'src/app/https/chart-http.service';
 import * as echarts from 'echarts';
-
+import { combineLatestAll } from 'rxjs';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 type EChartsOption = echarts.EChartsOption;
 type ECharts = echarts.ECharts;
 
@@ -17,6 +19,10 @@ export class ViewComponent implements OnInit {
   chartData: ECharts[] = [];
   chartDataOption: any = null;
   configOptionMaxMin: any;
+
+  table: any[] = [];
+
+  temp = 1;
   constructor(
     private $calculate: CalculateService,
     private $chart: ChartHttpService
@@ -27,20 +33,55 @@ export class ViewComponent implements OnInit {
       const chart1 = await this.$chart.get().toPromise();
       const chart2 = await this.$chart.get2().toPromise();
       const allDataChart = [...chart1, ...chart2];
+      const max = this.avgMaxModel(allDataChart);
+
       const optionCharts: EChartsOption[] = allDataChart.map((a: any) => {
+        const total = this.totalModel(a);
+        a.yAxis[1].max = Math.ceil(total) * 1.5;
+        // a.yAxis[0].splitNumber = 1;
+        a.yAxis[0].show = false;
+        a.yAxis[0].axisLabel = {
+          color: 'rgba(0, 0, 0, 1)',
+          fontWeight: 'bold',
+        };
+        a.yAxis[1].axisLabel = {
+          color: 'rgba(0, 0, 0, 1)',
+          fontWeight: 'bold',
+        };
+        a.xAxis[0].axisLabel = {
+          color: 'rgba(0, 0, 0, 1)',
+          fontWeight: 'bold',
+        };
+        a.xAxis[1].axisLabel = {
+          color: 'rgba(0, 0, 0, 1)',
+          fontWeight: 'bold',
+        };
+        a.legend = {
+          textStyle: {
+            // fontWeight: 'bold',
+            fontSize: 14,
+          },
+          padding: [
+            40, // up
+            10, // right
+            20, // down
+            10, // left
+          ],
+        };
+        // a.title.top = 2;
         a.series = a.series.map((s: any) => {
           if (s.type == 'bar') {
             s.barMaxWidth = 70;
             s.label = {
-              distance: 10,
+              // distance: 10,
               show: true,
               // offset: [0, -30],
-              position: 'right',
+              // position: 'center',
               // padding: [10, 10, 10, 10],
               formatter: function (param: any) {
                 return param.data ? param.data + '%' : param.data;
               },
-              fontSize: 8,
+              fontSize: 14,
               color: '#000',
               width: 100,
               height: 100,
@@ -48,76 +89,59 @@ export class ViewComponent implements OnInit {
             s.labelLine = {
               show: true,
             };
-            // s.barMinHeight = 50;
-            // s.labelLayout = function () {
-            //   return {
-            //     x: 10,
-            //     moveOverlap: 'shiftY',
-            //   };
-            // };
-            // if (s.name == 'Mat Cause') {
-            //   s.label.rotate = 80;
-            // }
-            // if (s.name == 'Array Cause') {
-            //   s.label.rotate = 60;
-            // }
-
-            // a.yAxis[0] = {
-            //   ...a.yAxis[0],
-            //   maxInterval: 10,
-            //   minInterval: 0.1,
-            //   min: 0,
-            //   max: 50,
-            //   scale: true,
-            //   axisLine: {
-            //     show: true,
-            //   },
-            // };
-            // a.yAxis[1] = {
-            //   ...a.yAxis[1],
-            //   maxInterval: 10,
-            //   minInterval: 0.1,
-            //   min: 0,
-            //   max: 50,
-            //   scale: true,
-            //   axisLine: {
-            //     show: true,
-            //   },
-            // };
-          } else {
+          }
+          if (s.type == 'line') {
             if (s.name == 'ST Yield') {
               s.label = {
                 show: true,
                 formatter: function (param: any) {
                   return param.data ? param.data + '%' : param.data;
                 },
-                fontSize: 8,
-                color: '#FF0000',
+                fontSize: 14,
                 fontWeight: 'bold',
+                color: '#000000',
               };
             } else if (s.name == 'Plan Yield') {
               s.label = {
-                show: true,
+                show: false,
                 formatter: function (param: any) {
                   return param.data ? param.data + '%' : param.data;
                 },
-                fontSize: 8,
-                color: '#000',
+                fontSize: 14,
+                color: '#FF0000',
               };
-              s.color = '#000';
+              s.lineStyle = {
+                type: 'dashed',
+                width: 1,
+              };
+              s.color = '#FF0000';
+            } else if (s.name == 'Target Pola FM') {
+              s.label = {
+                show: false,
+                formatter: function (param: any) {
+                  return param.data ? param.data + '%' : param.data;
+                },
+                fontSize: 14,
+                color: '#754519',
+              };
+              s.lineStyle = {
+                type: 'dotted',
+                width: 1.5,
+              };
+              s.color = '#00d066';
             } else {
               s.label = {
                 show: true,
                 formatter: function (param: any) {
                   return param.data ? param.data + '%' : param.data;
                 },
-                fontSize: 8,
+                fontSize: 14,
               };
             }
           }
-
           return s;
         });
+
         return a;
       });
       this.chartDataOption = optionCharts;
@@ -129,6 +153,8 @@ export class ViewComponent implements OnInit {
           };
         });
       });
+
+      this.table = this.createTable(optionCharts);
       setTimeout(() => {
         for (let index = 0; index < optionCharts.length; index++) {
           const option: EChartsOption = optionCharts[index];
@@ -140,6 +166,104 @@ export class ViewComponent implements OnInit {
     } catch (error) {
       console.log('🚀 ~ error:', error);
     }
+  }
+
+  createTable(options: any) {
+    return options.map((option: any) => {
+      const head = option.xAxis[0].data;
+      const seriesLen: number = option.series.length;
+      let firstColumn = [
+        'Plan Yield',
+        'ST Yield',
+        'Pola FM',
+        'Process Cause',
+        'Array Cause',
+        'Mat Cause',
+      ];
+      if (seriesLen === 7) {
+        firstColumn = [
+          'Plan Yield',
+          'ST Yield',
+          'Target Pola FM',
+          'Pola FM',
+          'Process Cause',
+          'Array Cause',
+          'Mat Cause',
+        ];
+      }
+      // const firstColumn = option.series.map((s: any) => s.name);
+      const body = firstColumn.map((f: any, i: number) => {
+        const item = option.series.find((s: any) => s.name == f);
+        const data = item.data.map((d: any) => (d ? d : ''));
+        let newFirstColumn = [
+          'Target yield',
+          'Yield',
+          'Polar FM',
+          'KTC process',
+          'Yasu array',
+          'Other',
+        ];
+        if (seriesLen === 7) {
+          newFirstColumn = [
+            'Target yield',
+            'Yield',
+            'Target Polar FM',
+            'Polar FM',
+            'KTC process',
+            'Yasu array',
+            'Other',
+          ];
+        }
+        return [newFirstColumn[i], ...data];
+      });
+
+      return {
+        head: ['Result', ...head],
+        body: body,
+      };
+    });
+  }
+  totalModel(options: any) {
+    const series = options.series;
+    const len = series[0].data.length;
+    const arr: any = Array.from({ length: len }, (_, index) => index);
+    return arr.reduce((p: any, n: any) => {
+      const a = series.reduce((p2: any, n2: any) => {
+        if (n2.type == 'bar') {
+          const value = Number(n2.data[n]);
+          p2 = p2 += value;
+        }
+        return p2;
+      }, 0);
+      a > p ? (p = a) : p;
+      return p;
+    }, 0);
+  }
+  avgMaxModel(options: any) {
+    let keys = [
+      'Mat Cause',
+      'Array Cause',
+      'Process Cause',
+      'Pola FM',
+      'Target Pola FM',
+    ];
+    const total = keys.map((key: any) => {
+      const max = options.reduce((p: any, n: any) => {
+        const series = n.series;
+        const itemSeries = series.find((s: any) => s.name == key);
+        if (itemSeries) {
+          let maxNumber = Math.max(...itemSeries.data);
+          if (p < maxNumber) p = maxNumber;
+        }
+        return p;
+      }, 0);
+      return {
+        key: key,
+        max: max,
+      };
+    });
+    const totalSort = total.sort((a: any, b: any) => b.max - a.max);
+    return totalSort[0].max;
   }
   ngAfterViewInit(): void {
     // alert();
@@ -156,5 +280,67 @@ export class ViewComponent implements OnInit {
     let option: any = this.chartData[i].getOption();
     option.yAxis[i_yAxis].min = value;
     this.chartData[i].setOption(option, false, true);
+  }
+  compareClass(key: any, tableBody: any, indexCol: number, value: any) {
+    if (indexCol >= 1) {
+      if (key == 'Yield') {
+        if (value >= tableBody[0][indexCol]) return 'text-green-500';
+        if (value < tableBody[0][indexCol]) return 'text-red-600';
+      }
+      if (key == 'Polar FM') {
+        if (tableBody.length === 7) {
+          if (value <= tableBody[2][indexCol]) return 'text-green-500';
+          if (value > tableBody[2][indexCol]) return 'text-red-600';
+        } else {
+          return 'text-red-600';
+        }
+      }
+      const foo = ['KTC process', 'Yasu array', 'Other'];
+      if (foo.some((a: any) => a == key)) {
+        return 'text-red-600';
+      }
+    }
+    return '';
+  }
+  handleAllMax(e: any, indexY: number) {
+    let value = Number(e.target.value);
+    this.chartData.map((chart: ECharts) => {
+      let option: any = chart.getOption();
+      option.yAxis[indexY].max = value;
+      chart.setOption(option, false, true);
+    });
+  }
+  handleAllMin(e: any, indexY: number) {
+    let value = Number(e.target.value);
+    this.chartData.map((chart: ECharts) => {
+      let option: any = chart.getOption();
+      option.yAxis[indexY].min = value;
+      chart.setOption(option, false, true);
+    });
+  }
+
+  handleExportPDF(i: number, name: any) {
+    const doc = new jsPDF({
+      orientation: 'landscape',
+      unit: 'mm',
+      compress: true,
+      format: 'a4',
+    });
+    let html: any = document.getElementById(`print${i}`);
+    html2canvas(html, {}).then(function (canvas) {
+      const image: any = canvas.toDataURL('png', 2);
+      doc.addImage(image, 'PNG', 10, 25, 280, 150);
+      doc.save(`${name}.pdf`);
+    });
+  }
+  handleExportIMG(i: number, name: any) {
+    let html: any = document.getElementById(`print${i}`);
+    html2canvas(html, {}).then(function (canvas) {
+      const image: any = canvas.toDataURL('png', 2);
+      const a = document.createElement('a');
+      a.setAttribute('download', `${name}.png`);
+      a.setAttribute('href', image);
+      a.click();
+    });
   }
 }
